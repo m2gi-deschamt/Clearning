2a94f058b62ff620633528d5fcd44a10ef25912a
m2gi-deschamt <omtd.deschamps@gmail.com>
2026-02-05 16:11:02 +0100

    add catch2 and the first test

---
+++ test/test_player.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch_test_macros.hpp>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+TEST_CASE("Addition simple", "[math]") {
+    REQUIRE(add(2, 3) == 5);
+    REQUIRE(add(-1, 1) == 0);
+    REQUIRE(add(0, 0) == 0);
+}
